9724d94cb5cc78f8ca48708f5387681ae8dba76d
Hinar-atAPE660 <linkjoestar402212@gmail.com>
2024-10-25 20:47:13 +0800

    esp32/espnow

--- Hardware/ESP32Part/include/espnow.h
+++ Hardware/ESP32Part/include/espnow.h
@@ -1,5 +1,5 @@
 #ifndef __ESPNOW_H
-#define __ESPMOW_H
+#define __ESPNOW_H
 
 #include <WiFi.h>
 #include <esp_now.h>
@@ -7,9 +7,35 @@
 #include "main.h"
 #include "gy30.h"
 
+/* initWiFi Function
+ * <> Initializes the WiFi connection for ESP-NOW
+ * <> 用于设置WiFi连接，以支持ESP-NOW功能
+*/
 void initWiFi();
+
+/* onSent Function
+ *
+ * <> Callback function when data is sent via ESP-NOW
+ *
+ * <> 数据发送完成后调用的回调函数
+ *
+ * <> 处理发送状态及相关的设备MAC地址
+ *
+ * @param mac_addr The MAC address of the device that was sent to
+ * @param status The status of the sent data
+*/
 void onSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+
+/* initESPNow Function
+ * <> Initializes ESP-NOW for communication
+ * <> 用于初始化ESP-NOW通信功能
+*/
 void initESPNow();
+
+/* sendESPNowData Function
+ * <> Sends data using ESP-NOW
+ * <> 使用ESP-NOW发送数据
+*/
 void sendESPNowData();
 
 #endif
--- Hardware/ESP32Part/src/espnow.cpp
+++ Hardware/ESP32Part/src/espnow.cpp
@@ -3,16 +3,35 @@
 // ESP-NOW PeerToPeer protocol
 uint8_t peerAddress[] = {0xCC, 0x50, 0xE3, 0x74, 0xF5, 0x1A};
 
+/* initWiFi Function
+ * <> Initializes the WiFi connection for ESP-NOW
+ * <> 用于设置WiFi连接，以支持ESP-NOW功能
+*/
 void initWiFi() {
     WiFi.mode(WIFI_STA);
     Serial.println("WiFi initialized in STA mode");
 }
 
+/* onSent Function
+ *
+ * <> Callback function when data is sent via ESP-NOW
+ *
+ * <> 数据发送完成后调用的回调函数
+ *
+ * <> 处理发送状态及相关的设备MAC地址
+ *
+ * @param mac_addr The MAC address of the device that was sent to
+ * @param status The status of the sent data
+*/
 void onSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
     Serial.print("\r\nLast Packet Send Status: ");
     Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
+/* initESPNow Function
+ * <> Initializes ESP-NOW for communication
+ * <> 用于初始化ESP-NOW通信功能
+*/
 void initESPNow() {
     initWiFi();
 
@@ -38,6 +57,10 @@ void initESPNow() {
     Serial.println("Peer added successfully");
 }
 
+/* sendESPNowData Function
+ * <> Sends data using ESP-NOW
+ * <> 使用ESP-NOW发送数据
+*/
 void sendESPNowData() {
     device.lightIntensity = (int)updateGY30();
     StaticJsonDocument<200> jsonDoc;
