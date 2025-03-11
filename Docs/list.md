94751bd47550ef15a7b326bf60bd946f9f1e7bc6
Hinar-atROG15 <linkjoestar402212@gmail.com>
2025-03-11 15:06:17 +0800

    list update

---
+++ Docs/list.md
@@ -0,0 +1,85 @@
+# 项目资料及清单
+
+## 目录组织结构
+
+**光照度控制系统**
+- [项目总体描述](#项目总体描述)
+    - 项目说明 | instruction
+- [当前子项目描述](#当前子项目描述)
+    - 子项目说明 | instruction
+- [项目成员](#项目成员)
+- [思维导图](#思维导图)
+- [工作分解结构WBS](#工作分解结构WBS)
+- [WBS词典](#WBS词典)
+- [项目的系列草图](#项目的系列草图)
+- [项目组成图](#项目组成图)
+- [体系结构图](#体系结构图)
+- [项目代办事项](#项目代办事项)
+- [冲刺待办事项](#冲刺待办事项)
+- [Project计划进度甘特图](#Project计划进度甘特图)
+
+## 项目总体描述
+
+**光照度控制系统 / CSDistrLighting**
+
+本项目是基于ESP芯片的分布式照明控制系统 (CSDL) ，包含配套仿真软件，提供办公、室内多场所的方案
+最终目标是完成分布式照明的主控制系统
+
+This Project is about the Control System of Distributed Lighting (CSDL) Based on ESP Chips, also conclude the simulation software, provide solutions for multiple locations in offices and indoor areas.  
+The final goal is to complete the Main Control System to the Distribute Lighting.
+
+
+## 当前子项目描述
+
+目前的进度是测试 "一灯一控" 平台的完成情况，并完成光照度控制系统 (CSDL) 的仿真软件 (CSilver) 的基本功能
+最终的目标是完成动态组合的分布式系统控制,包括多灯一控、多灯多控等自由度较高的定制方案
+
+Current status is to test the completed process of "ONE led and ONE master" platform, also complete the base function of the CSDL's Simulation(CSilver).
+The further goal is to complete the distribute system of dynamic compose, include "MULTIPLE led and ONE master" / "MULTIPLE led and MULTIPLE master" or other customized solutions as one like.
+
+## 项目成员
+
+| 成员 | 2025春 | 是否为组长 | 2024秋 | 是否为组长 | 2024春 | 是否为组长 |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| 黄翊轩 | :white_check_mark: | :white_check_mark: | :white_check_mark: | :x: | | |
+| 刘青青 | | | :white_check_mark: | :white_check_mark: | | |
+| 辛宇轩 | | | | |:white_check_mark: | :white_check_mark: |
+| 王承旭 | :white_check_mark: | :x: | :white_check_mark: | :x: | | |
+| 林嘉铭 | :white_check_mark: | :x: | :white_check_mark: | :x: | | |
+| 梁岳洋 | :white_check_mark: | :x: | | | | |
+| 柏麒 | :white_check_mark: | :x: | | | | |
+| 宋壮壮 | :white_check_mark: | :x: | | | | |
+| 宗嗣华 | | | | | :white_check_mark: | :x: |
+| 吴昊楠 | | | | | :white_check_mark: | :x: |
+| 孙懿 | | | | | :white_check_mark: | :x: |
+| 邓仕林 | | | | | :white_check_mark: | :x: |
+
+## 思维导图
+
+目前思维导图如下:
+![map](./Pic/光照度控制系统.png)
+
+附: [源文件](./光照度控制系统.mm)
+
+## 工作分解结构WBS
+
+## WBS词典
+
+## 项目的系列草图
+
+## 项目组成图
+
+## 体系结构图
+
+## 项目代办事项
+按当前紧迫性排序的与项目有关的全部工作
+
+## 冲刺待办事项
+按当前紧迫性排序的与项目有关的本学期全部工作
+
+## Project计划进度甘特图
+
+目前项目进度如下: 
+![gante](./Pic/gante.png)
+
+附: [**源文件**](./创新项目.mpp)
---
+++ Docs/光照度控制系统.mm
@@ -0,0 +1,100 @@
+
+<map>
+  <node ID="root" TEXT="光照度控制系统">
+    <node TEXT="1. 市场调研部分" ID="pF7oTSOUwu" _mubu_text="%3Cspan%20class=%22bold%22%3E1.%20%E5%B8%82%E5%9C%BA%E8%B0%83%E7%A0%94%E9%83%A8%E5%88%86%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="调研LED灯具市场" ID="vg9agCL7zR" _mubu_text="%3Cspan%20class=%22bold%22%3E%E8%B0%83%E7%A0%94LED%E7%81%AF%E5%85%B7%E5%B8%82%E5%9C%BA%3C/span%3E" STYLE="fork">
+        <node TEXT="产品功能对比" ID="Z9HxceR4xK" _mubu_text="%3Cspan%3E%E4%BA%A7%E5%93%81%E5%8A%9F%E8%83%BD%E5%AF%B9%E6%AF%94%3C/span%3E" STYLE="fork"/>
+        <node TEXT="成本与性能分析" ID="Cf4HhZpQ4b" _mubu_text="%3Cspan%3E%E6%88%90%E6%9C%AC%E4%B8%8E%E6%80%A7%E8%83%BD%E5%88%86%E6%9E%90%3C/span%3E" STYLE="fork"/>
+        <node TEXT="行业趋势分析" ID="MZbrJUPUma" _mubu_text="%3Cspan%3E%E8%A1%8C%E4%B8%9A%E8%B6%8B%E5%8A%BF%E5%88%86%E6%9E%90%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="调研调光技术" ID="AR9H3kdI8Z" _mubu_text="%3Cspan%20class=%22bold%22%3E%E8%B0%83%E7%A0%94%E8%B0%83%E5%85%89%E6%8A%80%E6%9C%AF%3C/span%3E" STYLE="fork">
+        <node TEXT="可控硅调光" ID="7eLW0LCXdJ" _mubu_text="%3Cspan%3E%E5%8F%AF%E6%8E%A7%E7%A1%85%E8%B0%83%E5%85%89%3C/span%3E" STYLE="fork"/>
+        <node TEXT="DALI（数字地址可调光照明接口）" ID="YoXPvpBcVm" _mubu_text="%3Cspan%3EDALI%EF%BC%88%E6%95%B0%E5%AD%97%E5%9C%B0%E5%9D%80%E5%8F%AF%E8%B0%83%E5%85%89%E7%85%A7%E6%98%8E%E6%8E%A5%E5%8F%A3%EF%BC%89%3C/span%3E" STYLE="fork"/>
+        <node TEXT="COB-LED技术" ID="04NtgRyJAN" _mubu_text="%3Cspan%3ECOB-LED%E6%8A%80%E6%9C%AF%3C/span%3E" STYLE="fork"/>
+        <node TEXT="RGBW（红绿蓝白）调光" ID="1jyDuTqin3" _mubu_text="%3Cspan%3ERGBW%EF%BC%88%E7%BA%A2%E7%BB%BF%E8%93%9D%E7%99%BD%EF%BC%89%E8%B0%83%E5%85%89%3C/span%3E" STYLE="fork"/>
+        <node TEXT="PWM（脉宽调制）调光技术" ID="ZJ8ZrDkthc" _mubu_text="%3Cspan%3EPWM%EF%BC%88%E8%84%89%E5%AE%BD%E8%B0%83%E5%88%B6%EF%BC%89%E8%B0%83%E5%85%89%E6%8A%80%E6%9C%AF%3C/span%3E" STYLE="fork"/>
+        <node TEXT="基于AI的自适应调光方案" ID="EzYEA6tQst" _mubu_text="%3Cspan%3E%E5%9F%BA%E4%BA%8EAI%E7%9A%84%E8%87%AA%E9%80%82%E5%BA%94%E8%B0%83%E5%85%89%E6%96%B9%E6%A1%88%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="2. 设计方案与优化" ID="3zLsb1akBz" _mubu_text="%3Cspan%20class=%22bold%22%3E2.%20%E8%AE%BE%E8%AE%A1%E6%96%B9%E6%A1%88%E4%B8%8E%E4%BC%98%E5%8C%96%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="电路设计" ID="1tIgGVUJ0s" _mubu_text="%3Cspan%20class=%22bold%22%3E%E7%94%B5%E8%B7%AF%E8%AE%BE%E8%AE%A1%3C/span%3E" STYLE="fork">
+        <node TEXT="确定电路架构" ID="QCme35aGo2" _mubu_text="%3Cspan%3E%E7%A1%AE%E5%AE%9A%E7%94%B5%E8%B7%AF%E6%9E%B6%E6%9E%84%3C/span%3E" STYLE="fork"/>
+        <node TEXT="低功耗优化方案（降低整体能耗）" ID="fQJtARrcUY" _mubu_text="%3Cspan%3E%E4%BD%8E%E5%8A%9F%E8%80%97%E4%BC%98%E5%8C%96%E6%96%B9%E6%A1%88%EF%BC%88%E9%99%8D%E4%BD%8E%E6%95%B4%E4%BD%93%E8%83%BD%E8%80%97%EF%BC%89%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="优化用户体验" ID="y1W6FI3xnt" _mubu_text="%3Cspan%20class=%22bold%22%3E%E4%BC%98%E5%8C%96%E7%94%A8%E6%88%B7%E4%BD%93%E9%AA%8C%3C/span%3E" STYLE="fork">
+        <node TEXT="学习Markdown语法" ID="r9yCK96mud" _mubu_text="%3Cspan%3E%E5%AD%A6%E4%B9%A0Markdown%E8%AF%AD%E6%B3%95%3C/span%3E" STYLE="fork"/>
+        <node TEXT="编写优化文档" ID="ou6w7bmaHD" _mubu_text="%3Cspan%3E%E7%BC%96%E5%86%99%E4%BC%98%E5%8C%96%E6%96%87%E6%A1%A3%3C/span%3E" STYLE="fork"/>
+        <node TEXT="UI/UX 交互优化（更直观的远程控制界面）" ID="a1Lucc1D3T" _mubu_text="%3Cspan%3EUI/UX%20%E4%BA%A4%E4%BA%92%E4%BC%98%E5%8C%96%EF%BC%88%E6%9B%B4%E7%9B%B4%E8%A7%82%E7%9A%84%E8%BF%9C%E7%A8%8B%E6%8E%A7%E5%88%B6%E7%95%8C%E9%9D%A2%EF%BC%89%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="组合硬件连接测试" ID="BT4uyd0SgA" _mubu_text="%3Cspan%3E%E7%BB%84%E5%90%88%E7%A1%AC%E4%BB%B6%E8%BF%9E%E6%8E%A5%E6%B5%8B%E8%AF%95%3C/span%3E" STYLE="fork"/>
+      <node TEXT="顶层与底层架构设计" ID="hhmlNHr6tX" _mubu_text="%3Cspan%20class=%22bold%22%3E%E9%A1%B6%E5%B1%82%E4%B8%8E%E5%BA%95%E5%B1%82%E6%9E%B6%E6%9E%84%E8%AE%BE%E8%AE%A1%3C/span%3E" STYLE="fork">
+        <node TEXT="顶层架构：" ID="SXzeCTwVF9" _mubu_text="%3Cspan%20class=%22bold%22%3E%E9%A1%B6%E5%B1%82%E6%9E%B6%E6%9E%84%3C/span%3E%3Cspan%3E%EF%BC%9A%3C/span%3E" STYLE="fork">
+          <node TEXT="描述不同应用场景下的功能需求" ID="uDW4V8lBK0" _mubu_text="%3Cspan%3E%E6%8F%8F%E8%BF%B0%E4%B8%8D%E5%90%8C%E5%BA%94%E7%94%A8%E5%9C%BA%E6%99%AF%E4%B8%8B%E7%9A%84%E5%8A%9F%E8%83%BD%E9%9C%80%E6%B1%82%3C/span%3E" STYLE="fork"/>
+          <node TEXT="远程APP调节亮度" ID="QKqKPXXwOR" _mubu_text="%3Cspan%3E%E8%BF%9C%E7%A8%8BAPP%E8%B0%83%E8%8A%82%E4%BA%AE%E5%BA%A6%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="底层实现：" ID="Hz9VOAYj7g" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%BA%95%E5%B1%82%E5%AE%9E%E7%8E%B0%3C/span%3E%3Cspan%3E%EF%BC%9A%3C/span%3E" STYLE="fork">
+          <node TEXT="初步搭建代码框架" ID="tKgXtGPdkT" _mubu_text="%3Cspan%3E%E5%88%9D%E6%AD%A5%E6%90%AD%E5%BB%BA%E4%BB%A3%E7%A0%81%E6%A1%86%E6%9E%B6%3C/span%3E" STYLE="fork"/>
+          <node TEXT="模块化开发，便于后期维护" ID="oIsB1Xt3Do" _mubu_text="%3Cspan%3E%E6%A8%A1%E5%9D%97%E5%8C%96%E5%BC%80%E5%8F%91%EF%BC%8C%E4%BE%BF%E4%BA%8E%E5%90%8E%E6%9C%9F%E7%BB%B4%E6%8A%A4%3C/span%3E" STYLE="fork"/>
+          <node TEXT="自适应调光（根据环境光自动调整亮度）" ID="xjFfkUbCA5" _mubu_text="%3Cspan%3E%E8%87%AA%E9%80%82%E5%BA%94%E8%B0%83%E5%85%89%EF%BC%88%E6%A0%B9%E6%8D%AE%E7%8E%AF%E5%A2%83%E5%85%89%E8%87%AA%E5%8A%A8%E8%B0%83%E6%95%B4%E4%BA%AE%E5%BA%A6%EF%BC%89%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="3. 硬件部分" ID="c5ds1lGaqw" _mubu_text="%3Cspan%20class=%22bold%22%3E3.%20%E7%A1%AC%E4%BB%B6%E9%83%A8%E5%88%86%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="ESP8266" ID="MGkmN0D04Y" _mubu_text="%3Cspan%20class=%22bold%22%3EESP8266%3C/span%3E" STYLE="fork">
+        <node TEXT="单点控制模块" ID="dekxsbSbM7" _mubu_text="%3Cspan%3E%E5%8D%95%E7%82%B9%E6%8E%A7%E5%88%B6%E6%A8%A1%E5%9D%97%3C/span%3E" STYLE="fork"/>
+        <node TEXT="扩展为多个节点" ID="fzsaopzqzW" _mubu_text="%3Cspan%3E%E6%89%A9%E5%B1%95%E4%B8%BA%E5%A4%9A%E4%B8%AA%E8%8A%82%E7%82%B9%3C/span%3E" STYLE="fork"/>
+        <node TEXT="控制不同区域灯具" ID="JRYclCjmKl" _mubu_text="%3Cspan%3E%E6%8E%A7%E5%88%B6%E4%B8%8D%E5%90%8C%E5%8C%BA%E5%9F%9F%E7%81%AF%E5%85%B7%3C/span%3E" STYLE="fork"/>
+        <node TEXT="通信方式：ESP-NOW 或 WiFi" ID="dLqZipimIh" _mubu_text="%3Cspan%3E%E9%80%9A%E4%BF%A1%E6%96%B9%E5%BC%8F%EF%BC%9AESP-NOW%20%E6%88%96%20WiFi%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="ESP32" ID="0ZlhfWmEba" _mubu_text="%3Cspan%20class=%22bold%22%3EESP32%3C/span%3E" STYLE="fork">
+        <node TEXT="环境光检测" ID="1HqRLJm6pT" _mubu_text="%3Cspan%3E%E7%8E%AF%E5%A2%83%E5%85%89%E6%A3%80%E6%B5%8B%3C/span%3E" STYLE="fork"/>
+        <node TEXT="数据采集并向自主控制节点传输" ID="4N3LbETcL6" _mubu_text="%3Cspan%3E%E6%95%B0%E6%8D%AE%E9%87%87%E9%9B%86%E5%B9%B6%E5%90%91%E8%87%AA%E4%B8%BB%E6%8E%A7%E5%88%B6%E8%8A%82%E7%82%B9%E4%BC%A0%E8%BE%93%3C/span%3E" STYLE="fork"/>
+        <node TEXT="支持数据分析与调节" ID="f0fxIf7MMr" _mubu_text="%3Cspan%3E%E6%94%AF%E6%8C%81%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90%E4%B8%8E%E8%B0%83%E8%8A%82%3C/span%3E" STYLE="fork"/>
+        <node TEXT="温湿度、空气质量传感器（实现更智能的灯光调节）" ID="GV2ZR5JYKj" _mubu_text="%3Cspan%3E%E6%B8%A9%E6%B9%BF%E5%BA%A6%E3%80%81%E7%A9%BA%E6%B0%94%E8%B4%A8%E9%87%8F%E4%BC%A0%E6%84%9F%E5%99%A8%EF%BC%88%E5%AE%9E%E7%8E%B0%E6%9B%B4%E6%99%BA%E8%83%BD%E7%9A%84%E7%81%AF%E5%85%89%E8%B0%83%E8%8A%82%EF%BC%89%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="uni-app" ID="pT4t2saRsY" _mubu_text="%3Cspan%20class=%22bold%22%3Euni-app%3C/span%3E" STYLE="fork">
+        <node TEXT="手机端控制" ID="c8PRRPzITJ" _mubu_text="%3Cspan%3E%E6%89%8B%E6%9C%BA%E7%AB%AF%E6%8E%A7%E5%88%B6%3C/span%3E" STYLE="fork"/>
+        <node TEXT="支持多节点可视化管理" ID="xK7DZLIUpx" _mubu_text="%3Cspan%3E%E6%94%AF%E6%8C%81%E5%A4%9A%E8%8A%82%E7%82%B9%E5%8F%AF%E8%A7%86%E5%8C%96%E7%AE%A1%E7%90%86%3C/span%3E" STYLE="fork"/>
+        <node TEXT="分支主题" ID="72Kfyzfu8W" _mubu_text="%3Cspan%3E%E5%88%86%E6%94%AF%E4%B8%BB%E9%A2%98%3C/span%3E" STYLE="fork"/>
+        <node TEXT="远程定时开关控制" ID="shXCNO1iVA" _mubu_text="%3Cspan%3E%E8%BF%9C%E7%A8%8B%E5%AE%9A%E6%97%B6%E5%BC%80%E5%85%B3%E6%8E%A7%E5%88%B6%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="4. 软件部分" ID="1VxmRdJuMl" _mubu_text="%3Cspan%20class=%22bold%22%3E4.%20%E8%BD%AF%E4%BB%B6%E9%83%A8%E5%88%86%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="ESP-NOW通信协议" ID="7KslVu1C0x" _mubu_text="%3Cspan%20class=%22bold%22%3EESP-NOW%E9%80%9A%E4%BF%A1%E5%8D%8F%E8%AE%AE%3C/span%3E" STYLE="fork">
+        <node TEXT="低延迟数据传输" ID="GTAZ204YKI" _mubu_text="%3Cspan%3E%E4%BD%8E%E5%BB%B6%E8%BF%9F%E6%95%B0%E6%8D%AE%E4%BC%A0%E8%BE%93%3C/span%3E" STYLE="fork"/>
+        <node TEXT="解决节点间“广播”问题" ID="iICW2P6wfx" _mubu_text="%3Cspan%3E%E8%A7%A3%E5%86%B3%E8%8A%82%E7%82%B9%E9%97%B4%E2%80%9C%E5%B9%BF%E6%92%AD%E2%80%9D%E9%97%AE%E9%A2%98%3C/span%3E" STYLE="fork"/>
+        <node TEXT="数据同步和一致性" ID="xw24MMSWo2" _mubu_text="%3Cspan%3E%E6%95%B0%E6%8D%AE%E5%90%8C%E6%AD%A5%E5%92%8C%E4%B8%80%E8%87%B4%E6%80%A7%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="数据可视化分析" ID="1iTsYfa5li" _mubu_text="%3Cspan%3E%E6%95%B0%E6%8D%AE%E5%8F%AF%E8%A7%86%E5%8C%96%E5%88%86%E6%9E%90%3C/span%3E" STYLE="fork"/>
+      <node TEXT="智能节能模式" ID="ROEZfASg22" _mubu_text="%3Cspan%3E%E6%99%BA%E8%83%BD%E8%8A%82%E8%83%BD%E6%A8%A1%E5%BC%8F%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="5. 扩展部分" ID="7yv6Byi3bE" _mubu_text="%3Cspan%20class=%22bold%22%3E5.%20%E6%89%A9%E5%B1%95%E9%83%A8%E5%88%86%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="多点数据整合与优化" ID="djG14Owq1m" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%A4%9A%E7%82%B9%E6%95%B0%E6%8D%AE%E6%95%B4%E5%90%88%E4%B8%8E%E4%BC%98%E5%8C%96%3C/span%3E" STYLE="fork">
+        <node TEXT="数据整合" ID="sU8uAG2vLP" _mubu_text="%3Cspan%3E%E6%95%B0%E6%8D%AE%E6%95%B4%E5%90%88%3C/span%3E" STYLE="fork">
+          <node TEXT="汇集领域节点数据" ID="zv4WdNmZhQ" _mubu_text="%3Cspan%3E%E6%B1%87%E9%9B%86%E9%A2%86%E5%9F%9F%E8%8A%82%E7%82%B9%E6%95%B0%E6%8D%AE%3C/span%3E" STYLE="fork"/>
+          <node TEXT="全局优化调整策略" ID="rE7HsORhC1" _mubu_text="%3Cspan%3E%E5%85%A8%E5%B1%80%E4%BC%98%E5%8C%96%E8%B0%83%E6%95%B4%E7%AD%96%E7%95%A5%3C/span%3E" STYLE="fork"/>
+          <node TEXT="云端数据存储和分析" ID="a9wXgFIuSu" _mubu_text="%3Cspan%3E%E4%BA%91%E7%AB%AF%E6%95%B0%E6%8D%AE%E5%AD%98%E5%82%A8%E5%92%8C%E5%88%86%E6%9E%90%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="负载均衡" ID="TE25ZGegLa" _mubu_text="%3Cspan%3E%E8%B4%9F%E8%BD%BD%E5%9D%87%E8%A1%A1%3C/span%3E" STYLE="fork">
+          <node TEXT="节点冗余" ID="2F6vUPZl5Y" _mubu_text="%3Cspan%3E%E8%8A%82%E7%82%B9%E5%86%97%E4%BD%99%3C/span%3E" STYLE="fork"/>
+          <node TEXT="确保稳定运行" ID="w3U3lWeBgn" _mubu_text="%3Cspan%3E%E7%A1%AE%E4%BF%9D%E7%A8%B3%E5%AE%9A%E8%BF%90%E8%A1%8C%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="多节点通信策略优化" ID="hfHUMjgCwA" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%A4%9A%E8%8A%82%E7%82%B9%E9%80%9A%E4%BF%A1%E7%AD%96%E7%95%A5%E4%BC%98%E5%8C%96%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="ESP-NOW优化" ID="JQCsIL3x09" _mubu_text="%3Cspan%20class=%22bold%22%3EESP-NOW%E4%BC%98%E5%8C%96%3C/span%3E" STYLE="fork">
+        <node TEXT="高效通信" ID="w8UVFeHpTJ" _mubu_text="%3Cspan%3E%E9%AB%98%E6%95%88%E9%80%9A%E4%BF%A1%3C/span%3E" STYLE="fork"/>
+        <node TEXT="数据同步" ID="12R3eagckE" _mubu_text="%3Cspan%3E%E6%95%B0%E6%8D%AE%E5%90%8C%E6%AD%A5%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="6. 实验与优化" ID="y7gOnlgSlw" _mubu_text="%3Cspan%20class=%22bold%22%3E6.%20%E5%AE%9E%E9%AA%8C%E4%B8%8E%E4%BC%98%E5%8C%96%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="测试与调优" ID="6DxVOMHrmX" _mubu_text="%3Cspan%20class=%22bold%22%3E%E6%B5%8B%E8%AF%95%E4%B8%8E%E8%B0%83%E4%BC%98%3C/span%3E" STYLE="fork">
+        <node TEXT="分布式布局的效果" ID="ozbHvkbVki" _mubu_text="%3Cspan%3E%E5%88%86%E5%B8%83%E5%BC%8F%E5%B8%83%E5%B1%80%E7%9A%84%E6%95%88%E6%9E%9C%3C/span%3E" STYLE="fork"/>
+        <node TEXT="调试节点响应时间与通信质量" ID="sRfFyKiSK1" _mubu_text="%3Cspan%3E%E8%B0%83%E8%AF%95%E8%8A%82%E7%82%B9%E5%93%8D%E5%BA%94%E6%97%B6%E9%97%B4%E4%B8%8E%E9%80%9A%E4%BF%A1%E8%B4%A8%E9%87%8F%3C/span%3E" STYLE="fork"/>
+        <node TEXT="根据实验结果优化算法与架构" ID="SuPJzeMn73" _mubu_text="%3Cspan%3E%E6%A0%B9%E6%8D%AE%E5%AE%9E%E9%AA%8C%E7%BB%93%E6%9E%9C%E4%BC%98%E5%8C%96%E7%AE%97%E6%B3%95%E4%B8%8E%E6%9E%B6%E6%9E%84%3C/span%3E" STYLE="fork"/>
+        <node TEXT="AI学习调光策略优化（基于用户行为调整亮度）" ID="0gGmesNHXJ" _mubu_text="%3Cspan%3EAI%E5%AD%A6%E4%B9%A0%E8%B0%83%E5%85%89%E7%AD%96%E7%95%A5%E4%BC%98%E5%8C%96%EF%BC%88%E5%9F%BA%E4%BA%8E%E7%94%A8%E6%88%B7%E8%A1%8C%E4%B8%BA%E8%B0%83%E6%95%B4%E4%BA%AE%E5%BA%A6%EF%BC%89%3C/span%3E" STYLE="fork"/>
+        <node TEXT="数据异常检测（自动发现传感器或灯光故障）" ID="gJZB7GUxiE" _mubu_text="%3Cspan%3E%E6%95%B0%E6%8D%AE%E5%BC%82%E5%B8%B8%E6%A3%80%E6%B5%8B%EF%BC%88%E8%87%AA%E5%8A%A8%E5%8F%91%E7%8E%B0%E4%BC%A0%E6%84%9F%E5%99%A8%E6%88%96%E7%81%AF%E5%85%89%E6%95%85%E9%9A%9C%EF%BC%89%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
